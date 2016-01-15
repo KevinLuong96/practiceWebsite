b4930c7e0dd63936098a2da507549a948ea8b2eb
Kevin Luong <k5luong@uwaterloo.ca>
2016-01-15 17:14:34 -0500

    inital commit

---
+++ script.js
@@ -0,0 +1,102 @@
+
+// function penis() {
+//   // body...
+//   document.getElementById("adopt-header").innerHTML= "penis";
+// }
+
+function tabbyText(){
+   document.getElementById("catContent").innerHTML=
+    "A tabby is any domestic cat that has a coat featuring distinctive  \
+    stripes, dots, lines or swirling patterns, usually together with a mark  \
+    resembling an 'M' on its forehead. Tabbies are sometimes erroneously  \
+    assumed to be a cat breed. In fact, the tabby pattern is found in  \
+    many breeds, as well as among the general mixed-breed population. The  \
+    tabby pattern is a naturally occurring feature that may be related to  \
+    the coloration of the domestic cat's direct ancestor, the African \
+    wildcat, which (along with the European wildcat and Asiatic wildcat) has  \
+    a similar coloration. ";
+
+
+}
+
+function siberianText(){
+  document.getElementById("catContent").innerHTML=
+    "The Siberian is a landrace variety of domestic cat, present in Russia for \
+    centuries, and more recently developed as a formal breed, with standards \
+    promulgated since the late 1980s. They are one of the largest known \
+    domesticated cats, second only to the maine coon. A longer name of the \
+    formal breed is Siberian Forest Cat, but it is usually referred to as the \
+    Siberian or the Siberian cat. Another name for the formal breed is the \
+    Moscow Semi-Longhair. The cat is an ancient breed \
+    that is now believed to be ancestral to all modern long-haired cats. The \
+    cat has similarities with the Norwegian Forest Cat, to which it is likely \
+    closely related. It is a natural breed of Siberia and the national cat of\
+    Russia.[citation needed] While it began as a landrace, it is selectively \
+    bred and pedigreed today in at least seven major cat fancier and breeder \
+    organisations. The colorpoint variant of the breed is called the \
+    Neva Masquerade by some registries, including Feline Federation Europe.";
+
+  document.getElementById("catpic").src = "http://www.cats.org.uk/uploads/images/pages/photo_latest14.jpg"
+//---------------------------TO DO----------------------------------------------
+}
+
+function sphinxText(){
+  document.getElementById("catContent").innerHTML=
+    "The Sphynx is a breed of cat developed through selective breeding starting \
+    in the 1960s, known for its lack of a coat (fur), though it is not truly \
+    hairless. The skin should have the texture of chamois, as it has fine hairs.\
+    Whiskers may be present, either whole or broken, or may be totally absent. \
+    The skin is the color their fur would be, and all the usual cat marking \
+    patterns (solid, point, van, tabby, tortie, etc.) may be found on Sphynx \
+    skin. Because they have no coat, they lose more body heat than coated cats. \
+    This makes them warm to the touch as well as heat-seeking.";
+}
+
+function siameseText(){
+  document.getElementById("catContent").innerHTML=
+    "The Siamese cat is one of the first distinctly recognized breeds of Oriental\
+    cat. One of several breeds native to Thailand (formerly known as Siam), \
+    the Siamese cat became one of the most popular breeds in Europe and \
+    North America in the 20th century. The modern Siamese is characterized by \
+    blue almond-shaped eyes, a triangular head shape, large ears, an elongated,\
+    slender, and muscular body, and point coloration. TICA describes the breed\
+    as social, intelligent, and playful into adulthood, often enjoying a game\
+    of fetch. Siamese tend to seek human interaction and also like \
+    companionship from other cats. The Oriental cat was developed in order to\
+    expand the range of coat patterns, while the Thai preserves a moderate\
+    head and body type.";
+}
+
+function bengalText(){
+  document.getElementById("catContent").innerHTML=
+    "The Bengal Cat is a hybrid breed of domestic cat developed by selective \
+    breeding and interspecific breeding to evoke the feline denizens of the \
+    jungle such as leopards, ocelots, margays and clouded leopards. Bengal Cats\
+    were developed by the selective breeding of domestic cats with the Asian\
+    leopard cat (ALC), Prionailurus bengalensis, with the goal of creating a \
+    confident, healthy and friendly cat with a highly contrasted and vividly \
+    marked coat. Bengal cats are generally a bright orange or light brown \
+    colour, although pale or off-white 'snow' Bengals also exist, and are\
+    popular among owners";
+}
+
+function scottishFoldText(){
+  document.getElementById("catContent").innerHTML=
+    "The Scottish Fold is a breed of domestic cat with a natural dominant-gene\
+    mutation that affects cartilage throughout the body, causing the ears to \
+    'fold', bending forward and down towards the front of their head, which \
+    gives the cat what is often described as an 'owl-like' appearance.\
+    Originally called lop-eared or lops after the lop-eared rabbit, \
+    Scottish Fold became the breed's name in 1966. Depending on registries, \
+    longhaired  Scottish Folds are varyingly known as Highland Fold, \
+    Scottish Fold Longhair, Longhair Fold and Coupari.";
+}
+
+function munchkinText(){
+  document.getElementById("catContent").innerHTML=
+    "The Munchkin is a relatively new breed of cat characterized by its very\
+    short legs, which are caused by a naturally occurring genetic mutation.\
+    Much controversy erupted over the breed when it was recognized by\
+    The International Cat Association in 1995 with critics voicing concern\
+    over potential health and mobility issues.";
+}
